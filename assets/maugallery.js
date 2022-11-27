64cbce8f8a0e6aa9982259c1861f5886f0702417
jaber sirraj <sir_jaber@yahoo.fr>
2022-11-27 21:41:07 +0000

    validator HTML 2

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -150,7 +150,8 @@
 
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i ;
+          //index = i ;
+          index = i -1;
         }
       });
       next =
@@ -189,7 +190,8 @@
 
       $(imagesCollection).each(function(i) {
         if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i;
+          //index = i;
+          index = i +1;
         }
       });
       next = imagesCollection[index] || imagesCollection[0];
@@ -239,8 +241,8 @@
       if ($(this).hasClass("active-tag")) {
         return;
       }
-      $(".active-tag").removeClass("active active-tag");
-      $(this).addClass("active-tag");
+      $(".active.active-tag").removeClass("active active-tag");
+      $(this).addClass("active active-tag");
 
       var tag = $(this).data("images-toggle");
 
